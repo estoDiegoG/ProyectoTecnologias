659001151c22e36535a7c614c32c9f2596b12af2
camiloamaya10 <90811848+camiloamaya10@users.noreply.github.com>
2021-10-13 19:43:34 -0500

    javascript

---
+++ Cotizacion.js
@@ -0,0 +1,11 @@
+//document.getElementsByTagName("input").onkeypress = function puntos(){return(charCode >= 48 && charCode <= 57)};
+//var onkeypress: (this: Window, ev: KeyboardEvent) => any
+// var inputs = document.getElementById(n);
+
+// function(){
+//     inputs.return(charCode >= 48 && charCode <= 57);
+//     // if(inputs[0]){
+//     //     inputs.onkeypress=(charCode >= 48 && charCode <= 57);
+//     //     return(event.charCode >= 48 && event.charCode <= 57);
+//     // }
+// }
